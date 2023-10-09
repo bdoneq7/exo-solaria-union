2488dcdbf9ac66f9981f89f74b524acd56872c6e
Brian Done <briguy2078@gmail.com>
2023-10-09 16:39:49 -0400

    added image to star and planet database

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,12 +6,14 @@ function About(props) {
   return (
     <section>
       <h1 className="mx-2" id="about">About Exo Solaria Union</h1>
-      
-      <div className="my-2 mx-2">
-      <img src={starfield} className="my-2" style={{ width: "20%", height: "20%" }} alt="" title="" />
-        <p>
+
+      <p>
         The Exo Solaria Union is dedicated to providing a list of exo planets and star systems, and encouraging the exploration and colonization of space.
         </p>
+      
+      <div className="my-2 mx-2">
+      <img src={starfield} className="my-2" style={{ width: "50%", height: "50%" }} alt="" title="" />
+        
       </div>
     </section>
   );
--- src/components/Stars/index.js
+++ src/components/Stars/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import starsystem from "../../assets/images/starsystem.jpg";
 
 
 
@@ -7,10 +8,15 @@ function Stars(props) {
     <section>
       <h1 className="mx-2" id="about">Star and Planet Database</h1>
       
-      <div className="my-2 mx-2">
-        <p>
-        Sol Star System
+      <p>
+        Sol Star System<br>
+        </br>
+        Proxima Centauri System
         </p>
+      
+      <div className="my-2 mx-2">
+      <img src={starsystem} className="my-2" style={{ width: "50%", height: "50%" }} alt="" title="" />
+
       </div>
     </section>
   );
