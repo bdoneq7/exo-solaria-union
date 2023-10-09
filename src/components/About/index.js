65acf785f07e99fe081ae74a204d8e0ff9b3c83d
Brian Done <briguy2078@gmail.com>
2023-10-09 16:31:56 -0400

    added starfield image

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import starfield from "../../assets/images/starfield.jpg";
 
 
 function About(props) {
@@ -7,6 +8,7 @@ function About(props) {
       <h1 className="mx-2" id="about">About Exo Solaria Union</h1>
       
       <div className="my-2 mx-2">
+      <img src={starfield} className="my-2" style={{ width: "20%", height: "20%" }} alt="" title="" />
         <p>
         The Exo Solaria Union is dedicated to providing a list of exo planets and star systems, and encouraging the exploration and colonization of space.
         </p>
