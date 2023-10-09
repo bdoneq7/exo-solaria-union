c27f699d9e87e8b2a01ef6237a2361df9ac7bebc
Brian Done <briguy2078@gmail.com>
2023-10-09 17:25:31 -0400

    changed logo image size

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Nav from "../Nav";
-import logo from "../../assets/images/logo.png";
+import logo from "../../assets/images/logo-small.png";
 
 function Header(props) {
 	const { currentTab, setCurrentTab } = props;
@@ -8,7 +8,7 @@ function Header(props) {
 	return (
 		<header>
 			<div class="logodiv">
-			<img src={logo} className="my-2" style={{ width: "20%", height: "20%" }} alt="Exo Solaria Union" title="Exo Solaria Union" />
+			<img src={logo} className="my-2" style={{ width: "100%", height: "100%" }} alt="Exo Solaria Union" title="Exo Solaria Union" />
 			</div>
 			<div class="linksdiv">
 				<Nav
